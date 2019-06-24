258ddf34f5aed0f931e4e1210f5ddfcf373b0d74
pennypesh <pennydehoney@yahoo.com>
2019-06-24 10:08:38 -0500

    changes on jobController

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -6,6 +6,7 @@ import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,8 +27,8 @@ public class JobController {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
 
-        Job job=jobData.findById(id);
-        model.addAttribute("job",job);
+        Job job = jobData.findById(id);
+        model.addAttribute("job", job);
 
         return "job-detail";
     }
@@ -39,19 +40,24 @@ public class JobController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @Valid JobForm jobForm, Errors errors) {
+    public String add(Model model,@ModelAttribute Job newJob, @Valid JobForm jobForm, Errors errors) {
 
         // TODO #6 - Validate the JobForm model, and if valid, create a
         // new Job and add it to the jobData data store. Then
-        // redirect to the job detail view for the new Job.
-        if(errors.hasErrors()){
+//         redirect to the job detail view for the new Job.
+        if (errors.hasErrors()) {
             return "new-job";
         }
+        model.addAttribute("jobForm",jobForm);
 
+        newJob.setName(jobForm.getName());
+        newJob.setEmployer(jobData.getEmployers().findById(jobForm.getEmployerId()));
+        newJob.setLocation(jobData.getLocations().findById(jobForm.getLocationId()));
+        newJob.setCoreCompetency(jobData.getCoreCompetencies().findById(jobForm.getCoreCompetencyId()));
+        newJob.setPositionType(jobData.getPositionTypes().findById(jobForm.getPositionTypeId()));
 
+        jobData.add(newJob);
 
-
-        return "";
-
+        return "redirect:?id=" + newJob.getId();
     }
 }
